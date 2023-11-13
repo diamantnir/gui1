1b48da6f5b7db464f4c6d5439362789a8bdf3aed
diamantnir <diamantnir@gmail.com>
2023-11-13 12:52:58 +0200

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,43 @@
+export const environment = {
+  production: true,
+
+  //apiUrl: 'http://192.168.1.122:82'
+  //apiUrl:'http://localhost:2587/'
+
+  // apiUrl: 'http://192.168.1.122:82',
+
+   //apiUrl:'https://amitecmesdev.amitec-solutions.com',
+ //apiUrl: 'http://agamiapi.amitec-solutions.com',
+ //apiUrl: 'http://plas-fitapi.amitec-solutions.com',
+ managerPassword: 247,
+
+
+  // apiUrl: 'http://ackmessrv:82'
+  // apiUrl: 'http://10.1.1.19:82'
+  //  apiUrl: 'http://ackmessrv:82'
+
+
+//apiUrl: 'http://10.0.0.235/MesApi',
+
+  //apiUrl:'http://192.168.110.7:82' // sanlakol
+  //apiUrl: 'http://192.168.1.196:82'
+  //  apiUrl: 'http://192.168.110.7:82', // sanlakol
+
+  //  apiUrl: 'http://192.168.110.7:85', // sanlakol test
+
+  //   apiUrl: 'http://192.168.110.7:85', // sanlakol test
+  userId: 0,
+
+  //apiUrl: 'http://10.0.0.2:82'
+
+  //apiUrl: 'http://192.168.1.196:82'
+  // apiUrl:'http://192.168.110.7:82' // sanlakol
+  //apiUrl: 'http://ILBSWTVZETES:82' // adama
+  //apiUrl:'http://ilbswtvzetes/ZetesOeeApi' // adama same port
+
+ apiUrl:'https://agamiapi.amitec-solutions.com' // agami
+ //apiUrl:'http://amitecmesdev.amitec-solutions.com' // test
+
+
+
+};
---
+++ environments/environment.ts
@@ -0,0 +1,33 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  // apiUrl: 'http://localhost:82'
+  // apiUrl: 'http://10.0.0.2:82'
+
+
+  // apiUrl: 'http://localhost:2573'
+  apiUrl: 'http://localhost:2587',
+  userId: 0,
+  managerPassword: 247
+  // apiUrl:'http://amitecmesdev.amitec-solutions.com'
+  // apiUrl: 'http://AshushApi.amitec-solutions.com'
+  // apiUrl: 'http://192.168.1.122:82'
+  // apiUrl: 'http://ackmessrv:82',
+
+
+  // apiUrl: 'http://10.0.0.2:82',
+  // apiUrl: 'http://localhost:2582',
+  // apiUrl: 'http://amitecmesdev.amitec-solutions.com'
+
+
+  // apiUrl: 'http://localhost:2582',
+  // apiUrl: 'http://amitecmesdev.amitec-solutions.com'
+
+
+
+
+};
